72894842878587fba6ec32c3869b049b37a6cb04
baran polat <baran488@outlook.com>
2019-12-09 17:46:23 +0300

    Add project files.

---
+++ LinqToExamples/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace LinqToExamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Example = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1029, 339);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(101, 29);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Example 9";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(932, 339);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(101, 29);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "Example 8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(833, 339);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(101, 29);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Example 7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(732, 339);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 29);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Example 6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(631, 339);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 29);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Example 5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(529, 339);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Example 4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(424, 339);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 29);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Example 3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Example
+            // 
+            this.Example.Location = new System.Drawing.Point(323, 339);
+            this.Example.Name = "Example";
+            this.Example.Size = new System.Drawing.Size(101, 29);
+            this.Example.TabIndex = 9;
+            this.Example.Text = "Example 2";
+            this.Example.UseVisualStyleBackColor = true;
+            this.Example.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(221, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Example 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-91, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(221, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(883, 199);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(631, 398);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(101, 29);
+            this.button15.TabIndex = 17;
+            this.button15.Text = "Example 14";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(529, 398);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(101, 29);
+            this.button16.TabIndex = 18;
+            this.button16.Text = "Example 13";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(424, 398);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(101, 29);
+            this.button17.TabIndex = 19;
+            this.button17.Text = "Example 12";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(323, 398);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(101, 29);
+            this.button18.TabIndex = 20;
+            this.button18.Text = "Example 11";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(221, 398);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(101, 29);
+            this.button19.TabIndex = 21;
+            this.button19.Text = "Example 10";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 563);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Example);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button Example;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ LinqToExamples/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToExamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NorthwindEntities db = new NorthwindEntities();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.OrderBy(x => x.ProductName).ToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Where(x => SqlFunctions.DateDiff("YEAR", x.BirthDate, DateTime.Now) > 60).Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.BirthDate,
+
+
+            }).ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = db.Order_Details.OrderByDescending(x => x.UnitPrice).Select(x => new
+            {
+                x.Order,
+                x.Product,
+                x.UnitPrice,
+
+            }).Where(x => x.UnitPrice > 50).ToList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.OrderBy(x => x.BirthDate).Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.Title,
+                Age = (SqlFunctions.DateDiff("YEAR", x.BirthDate, DateTime.Now))
+
+            }).ToList();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Category category = db.Categories.First(x => x.CategoryID > 7);
+                MessageBox.Show(category.CategoryName);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is no such category!");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            Category category = db.Categories.Find(1);
+            MessageBox.Show($"Category Name: {category.CategoryName}\nDescreption: {category.Description}");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.Where(x => x.UnitsInStock >= 20 && x.UnitsInStock <= 60).Take(5).OrderByDescending(x => x.UnitPrice).ToList();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.OrderByDescending(x => x.UnitsInStock).Skip(5).Take(10).Select(x => new
+            {
+                x.ProductName,
+                x.UnitsInStock,
+                x.UnitPrice,
+                x.Category,
+
+            }).ToList();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Categories.Where(x => x.Description.Contains("drinks")).ToList();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Where(x => x.FirstName.StartsWith("A")).OrderByDescending(x => x.BirthDate).ToList();
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.ProductID > 6);
+
+            if (product == null)
+            {
+                MessageBox.Show("There is no such product!");
+            }
+            else
+            {
+                MessageBox.Show(product.ProductName);
+            }
+        }
+
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.Where(x => x.ProductName.EndsWith("e")).ToList();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.GroupBy(x => x.Category.CategoryName).Select(y => new
+            {
+                CategoryName = y.Key,
+                UnitInStock = y.Sum(x => x.UnitsInStock)
+            }).ToList();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            bool result = db.Products.Any(x => x.UnitPrice > 100);
+            MessageBox.Show(result.ToString());
+        }
+
+    
+    }
+}
